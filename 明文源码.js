a9f265555c3d5fa42e16c2a3e0b86b4d4c3a54b4
mesment <mesment7826@gmail.com>
2024-12-25 01:17:06 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'cc28790c-7fce-45e2-81be-70b0ec2e3a41';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
